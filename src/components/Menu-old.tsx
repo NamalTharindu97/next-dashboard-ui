e3310a263a6ff72d3a221181b3e57cf26609c41e
NamalTharindu97 <NamalA701997@gmail.com>
2025-01-05 11:40:02 +0530

    menu bar

--- src/components/Menu-old.tsx
+++ src/components/Menu-old.tsx
@@ -1,6 +1,3 @@
-import Image from "next/image";
-import Link from "next/link";
-
 const menuItems = [
   {
     title: "MENU",
@@ -115,29 +112,3 @@ const menuItems = [
     ],
   },
 ];
-
-const Menu = () => {
-  return (
-    <div className="mt-4 text-sm">
-      {menuItems.map((i) => (
-        <div className="flex flex-col gap-2" key={i.title}>
-          <span className="text-gray-400 hidden lg:block font-light my-4">
-            {i.title}
-          </span>
-          {i.items.map((item) => (
-            <Link
-              className="flex items-center justify-center lg:justify-start gap-4 text-gray-500 py-2"
-              href={item.href}
-              key={item.label}
-            >
-              <Image src={item.icon} alt="" width={20} height={20} />
-              <span className="hidden lg:block">{item.label}</span>
-            </Link>
-          ))}
-        </div>
-      ))}
-    </div>
-  );
-};
-
-export default Menu;
